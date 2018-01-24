a0ae56c8cfc4e5d1e681b15cc4e1839ee564979f
JOSEEDUARDO <JOSEEDUARDO@10.20.7.11>
2018-01-23 21:38:50 -0600

    Elementos Clase 23/01/18 Campos Juarez Jose Eduardo

---
+++ src/main/java/unitec/org/elementos/tablitas.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  JOSEEDUARDO
+ * Created: 23/01/2018
+ */
+
+create table mensajitos (id integer primary key auto_increment,
+                         titulo varchar(40),
+                         cuerpo varchar(180));
